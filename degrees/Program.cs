82b9b34e444047116d3f91139230034d8ca0a43b
Евгений <@EUGENE1984>
2023-10-29 13:16:31 +0500

    Добавьте файлы проекта.

---
+++ degrees/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Выберите опцию (1 - Фаренгейт в Цельсий, 2 - Цельсий в Фаренгейт): ");
+        int choice = int.Parse(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            Console.Write("Введите температуру в Фаренгейтах: ");
+            double fahrenheit = double.Parse(Console.ReadLine());
+            double celsius = FahrenheitToCelsius(fahrenheit);
+            Console.WriteLine($"{fahrenheit} градусов Фаренгейта равно {celsius:F2} градусов Цельсия");
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Введите температуру в Цельсиях: ");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = CelsiusToFahrenheit(celsius);
+            Console.WriteLine($"{celsius} градусов Цельсия равно {fahrenheit:F2} градусов Фаренгейта");
+        }
+        else
+        {
+            Console.WriteLine("Неправильный выбор опции.");
+        }
+    }
+}
---
+++ numbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число от 1 до 100: ");
+        string input = Console.ReadLine();
+        int number = 0;
+
+        if (input.Length > 0 && input.Length < 4)
+        {
+            bool isNumber = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsDigit(input[i]))
+                {
+                    isNumber = false;
+                    break;
+                }
+            }
+
+            if (isNumber)
+            {
+                number = int.Parse(input);
+
+                if (number >= 1 && number <= 100)
+                {
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        Console.WriteLine("Fizz Buzz");
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (number % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: число вне диапазона от 1 до 100");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введено некорректное число");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: введено некорректное число");
+        }
+    }
+}
